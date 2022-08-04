f7258326dea658d31b14c43352ce74128e9c963e
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-08-04 10:02:47 -0400

    init

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+// pages/_app.js
+import { ChakraProvider } from "@chakra-ui/react";
+
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme();
+
+export default theme;
