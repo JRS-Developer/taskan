0cbe99038439dacefaffba475875a117b9c975d1
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-08-09 12:21:22 -0400

    feat: some dark mode improvementes for the navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -7,23 +7,41 @@ import {
   FormLabel,
   Heading,
   Input,
+  Link,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   Spacer,
   Text,
+  useColorModeValue,
   VisuallyHidden,
 } from "@chakra-ui/react";
 import { signOut, useSession } from "next-auth/react";
 import { HiAdjustments, HiChevronDown, HiOutlineLogout } from "react-icons/hi";
+import NextLink from "next/link";
 
 const Navbar = () => {
   const { data } = useSession();
+  const navBg = useColorModeValue("gray.50", "gray.900");
 
   return (
-    <Flex as="nav" py="4" px="8" bg="gray.900" shadow="xl">
-      <Heading as="h1">Taskan</Heading>
+    <Flex
+      as="nav"
+      py="4"
+      px="8"
+      bg={navBg}
+      shadow="base"
+      align="center"
+      h="full"
+    >
+      <NextLink href="/" passHref>
+        <Link>
+          <Heading as="h1" size="lg">
+            Taskan
+          </Heading>
+        </Link>
+      </NextLink>
 
       <Spacer />
 
