50cc0b055c5867662377624ea0a2ab822f901b47
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-08-11 21:02:20 -0400

    feat: added route to delete columns by Id

--- src/server/router/columns.ts
+++ src/server/router/columns.ts
@@ -3,31 +3,59 @@ import { z } from "zod";
 import { TRPCError } from "@trpc/server";
 import { prisma } from "@/server/db/client";
 
-export const columnsRouter = createProtectedRouter().mutation("createOne", {
-  input: z.object({
-    name: z.string().min(1),
-    boardId: z.string().uuid(),
-  }),
-  async resolve({ input }) {
-    // check if the board exits
-    const board = await prisma.board.findUnique({
-      where: {
-        id: input.boardId,
-      },
-    });
-
-    if (!board) {
-      throw new TRPCError({
-        message: "The board doesn't exits",
-        code: "NOT_FOUND",
+export const columnsRouter = createProtectedRouter()
+  .mutation("createOne", {
+    input: z.object({
+      name: z.string().min(1),
+      boardId: z.string().uuid(),
+    }),
+    async resolve({ input }) {
+      // check if the board exits
+      const board = await prisma.board.findUnique({
+        where: {
+          id: input.boardId,
+        },
       });
-    }
 
-    // create column
-    const column = await prisma.boardList.create({
-      data: input,
-    });
+      if (!board) {
+        throw new TRPCError({
+          message: "The board doesn't exits",
+          code: "NOT_FOUND",
+        });
+      }
 
-    return column;
-  },
-});
+      // create column
+      const column = await prisma.boardList.create({
+        data: input,
+      });
+
+      return column;
+    },
+  })
+  .mutation("deleteById", {
+    input: z.object({
+      id: z.string().uuid(),
+    }),
+    async resolve({ input: { id } }) {
+      // check if the column exits
+      const column = await prisma.boardList.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!column) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "This column doesn't exit",
+        });
+      }
+
+      // delete column if it exits
+      return await prisma.boardList.delete({
+        where: {
+          id,
+        },
+      });
+    },
+  });
