e1b319a12845b4ba6cf4c687a79ee5aa941021b9
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-08-11 21:04:50 -0400

    feat: made improvements for the form to create cards and columns

--- src/components/BoardColumns/BoardColumnForm.tsx
+++ src/components/BoardColumns/BoardColumnForm.tsx
@@ -7,55 +7,85 @@ import {
   FormLabel,
   Input,
   Box,
+  FormErrorMessage,
 } from "@chakra-ui/react";
-import { FormEvent, LegacyRef } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormEvent, LegacyRef, useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { HiX } from "react-icons/hi";
+import { z } from "zod";
 
 type Props = {
   isLoading: boolean;
   onCancel: () => any;
-  handleSubmit: (e: FormEvent<HTMLFormElement>) => void;
-  inputRef?: LegacyRef<HTMLInputElement>;
+  onSubmit: SubmitHandler<FormData>;
   inputPlaceholder: string;
   label: string;
   addButtonText: string;
 };
 
+const schema = z.object({
+  name: z.string().min(1, {
+    message: "The name is required",
+  }),
+});
+
+type FormData = z.infer<typeof schema>;
+
 const BoardColumnForm = ({
-  handleSubmit,
+  onSubmit,
   isLoading,
   onCancel,
-  inputRef,
   inputPlaceholder,
   label,
   addButtonText,
-}: Props) => (
-  <form
-    onSubmit={(e) => {
-      e.preventDefault();
-      handleSubmit(e);
-    }}
-  >
-    <Box border="1px" shadow="md" rounded="md" p="2">
-      <FormControl>
-        <VisuallyHidden>
-          <FormLabel>{label}</FormLabel>
-        </VisuallyHidden>
-        <Input ref={inputRef} placeholder={inputPlaceholder} />
-      </FormControl>
-      <HStack mt="2">
-        <Button colorScheme="blue" type="submit" isLoading={isLoading}>
-          {addButtonText}
-        </Button>
-        <IconButton
-          aria-label="Cancel"
-          disabled={isLoading}
-          onClick={onCancel}
-          icon={<HiX />}
-        />
-      </HStack>
-    </Box>
-  </form>
-);
+}: Props) => {
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+    setFocus,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  useEffect(() => {
+    setFocus("name");
+  }, [setFocus]);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Box border="1px" shadow="md" rounded="md" p="2">
+        <FormControl isInvalid={Boolean(errors.name)}>
+          <VisuallyHidden>
+            <FormLabel htmlFor="name">{label}</FormLabel>
+          </VisuallyHidden>
+          <Input
+            id="name"
+            placeholder={inputPlaceholder}
+            {...register("name")}
+          />
+          <FormErrorMessage>
+            {Boolean(errors.name) && errors.name?.message}
+          </FormErrorMessage>
+        </FormControl>
+        <HStack mt="2">
+          <Button colorScheme="blue" type="submit" isLoading={isLoading}>
+            {addButtonText}
+          </Button>
+          <IconButton
+            aria-label="Cancel"
+            disabled={isLoading}
+            onClick={onCancel}
+            icon={<HiX />}
+          />
+        </HStack>
+      </Box>
+    </form>
+  );
+};
 
 export default BoardColumnForm;
