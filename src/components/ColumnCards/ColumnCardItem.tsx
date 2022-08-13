be7e1b421e13b5e8f7baec3fbf806d170c7a262d
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-08-12 20:24:47 -0400

    feat: now when the user clicks a card, it shows the modal

---
+++ src/components/ColumnCards/ColumnCardItem.tsx
@@ -0,0 +1,60 @@
+import { Flex, HStack, IconButton, Tag, Text, Tooltip } from "@chakra-ui/react";
+import Image from "next/image";
+import { HiPlus } from "react-icons/hi";
+import { BoardCardByIdT } from "@/types/trpc-queries";
+import { useCardModalStore } from "@/store";
+
+type Props = {
+  card: BoardCardByIdT;
+};
+
+const ColumnCardItem = ({ card: { id, cover, name, labels } }: Props) => {
+  const openModal = useCardModalStore((state) => state.open);
+
+  return (
+    <Flex
+      key={id}
+      bg="white"
+      direction="column"
+      p="3"
+      rounded="xl"
+      shadow="base"
+      gap="3"
+      onClick={() => openModal(id)}
+      transition="box-shadow 0.2s"
+      _hover={{
+        cursor: "pointer",
+        shadow: "md",
+        transition: "box-shadow 0.2s",
+      }}
+    >
+      {cover && (
+        <Flex position="relative" h="130px" w="220px">
+          <Image src={cover} alt={name} layout="fill" objectFit="cover" />
+        </Flex>
+      )}
+      <Text size="md">{name}</Text>
+      {labels.length > 0 && (
+        <HStack>
+          {labels.map(({ Label }) => (
+            <Tag key={`${id}-${Label.id}`}>{Label.id}</Tag>
+          ))}
+        </HStack>
+      )}
+      <Flex>
+        <Flex>
+          <Tooltip label="Add member">
+            <IconButton
+              icon={<HiPlus />}
+              aria-label="Add member"
+              colorScheme="blue"
+              size="sm"
+            />
+          </Tooltip>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default ColumnCardItem;
--- src/components/ColumnCards/index.tsx
+++ src/components/ColumnCards/index.tsx
@@ -1,7 +1,5 @@
 import { BoardCardByIdT } from "@/types/trpc-queries";
-import { Flex, HStack, IconButton, Tag, Text, Tooltip } from "@chakra-ui/react";
-import Image from "next/image";
-import { HiPlus } from "react-icons/hi";
+import ColumnCardItem from "./ColumnCardItem";
 
 type Props = {
   cards: BoardCardByIdT[];
@@ -10,42 +8,8 @@ type Props = {
 const ColumnCards = ({ cards }: Props) => {
   return (
     <>
-      {cards.map(({ id, name, labels, cover }) => (
-        <Flex
-          key={id}
-          bg="white"
-          direction="column"
-          p="3"
-          rounded="xl"
-          shadow="base"
-          gap="3"
-        >
-          {cover && (
-            <Flex position="relative" h="130px" w="220px">
-              <Image src={cover} alt={name} layout="fill" objectFit="cover" />
-            </Flex>
-          )}
-          <Text size="md">{name}</Text>
-          {labels.length > 0 && (
-            <HStack>
-              {labels.map(({ Label }) => (
-                <Tag key={`${id}-${Label.id}`}>{Label.id}</Tag>
-              ))}
-            </HStack>
-          )}
-          <Flex>
-            <Flex>
-              <Tooltip label="Add member">
-                <IconButton
-                  icon={<HiPlus />}
-                  aria-label="Add member"
-                  colorScheme="blue"
-                  size="sm"
-                />
-              </Tooltip>
-            </Flex>
-          </Flex>
-        </Flex>
+      {cards.map((card) => (
+        <ColumnCardItem key={card.id} card={card} />
       ))}
     </>
   );
