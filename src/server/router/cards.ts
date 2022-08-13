47bffe86c8842f4d96a217ee730f13bf377ac12f
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-08-12 20:23:31 -0400

    feat: created route to query one card by id

--- src/server/router/cards.ts
+++ src/server/router/cards.ts
@@ -4,39 +4,64 @@ import { prisma } from "@/server/db/client";
 import { TRPCError } from "@trpc/server";
 import getLastPosition from "@/utils/getLastPosition";
 
-export const cardsRouter = createProtectedRouter().mutation("createOne", {
-  input: z.object({
-    columnId: z.string().uuid(),
-    name: z.string().min(1),
-  }),
-  async resolve({ input }) {
-    const boardColumn = await prisma.boardList.findUnique({
-      where: {
-        id: input.columnId,
-      },
-      include: {
-        cards: true,
-      },
-    });
-
-    // check that the boardColumn exits
-    if (!boardColumn)
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message: "The board column doesn't exist",
+export const cardsRouter = createProtectedRouter()
+  .query("getOneById", {
+    input: z.object({
+      id: z.string().uuid(),
+    }),
+    async resolve({ input }) {
+      const card = await prisma.card.findUnique({
+        where: {
+          id: input.id,
+        },
+        include: {
+          boardList: true,
+        },
       });
 
-    // get last position
-    const lastPosition = getLastPosition(boardColumn.cards);
+      // returns error if the card doesn't exit
+      if (!card)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "The card doesn't exit",
+        });
 
-    const card = await prisma.card.create({
-      data: {
-        name: input.name,
-        boardListId: input.columnId,
-        position: lastPosition + 1,
-      },
-    });
+      return card;
+    },
+  })
+  .mutation("createOne", {
+    input: z.object({
+      columnId: z.string().uuid(),
+      name: z.string().min(1),
+    }),
+    async resolve({ input }) {
+      const boardColumn = await prisma.boardList.findUnique({
+        where: {
+          id: input.columnId,
+        },
+        include: {
+          cards: true,
+        },
+      });
 
-    return card;
-  },
-});
+      // check that the boardColumn exits
+      if (!boardColumn)
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "The board column doesn't exist",
+        });
+
+      // get last position
+      const lastPosition = getLastPosition(boardColumn.cards);
+
+      const card = await prisma.card.create({
+        data: {
+          name: input.name,
+          boardListId: input.columnId,
+          position: lastPosition + 1,
+        },
+      });
+
+      return card;
+    },
+  });
--- src/types/trpc-queries.ts
+++ src/types/trpc-queries.ts
@@ -1,7 +1,9 @@
 import { inferQueryOutput } from "@/utils/trpc";
 
+// Board types
 export type BoardByIdT = inferQueryOutput<"boards.getOneById">;
-
 export type BoardColumnByIdT = BoardByIdT["lists"][0];
-
 export type BoardCardByIdT = BoardColumnByIdT["cards"][0];
+
+// Card types
+export type CardByIdT = inferQueryOutput<"cards.getOneById">;
